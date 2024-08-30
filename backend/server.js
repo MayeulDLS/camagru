f9b0b5db381f534f961fde730c91e56ba2d230f6
Mayeul DE LA SERRE <mde-la-s@made-f0Ar3s5.clusters.42paris.fr>
2024-08-30 15:00:12 +0200

    backend init

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+const { normalizePort, errorHandler } = require('./errorHandling');
+
+// Charger les variables d'environnement à partir d'un fichier .env (facultatif)
+dotenv.config();
+
+// Normaliser le port
+const port = normalizePort(process.env.PORT);
+app.set('port', port);
+
+// Créer le serveur HTTP
+const server = http.createServer(app);
+
+// Gérer les erreurs
+server.on('error', errorHandler);
+
+// Écouter l'événement 'listening'
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+// Connexion à MongoDB et démarrage du serveur
+//const mongoUri = process.env.MONGO_URI;
+//mongoose.connect(mongoUri, {
+//  useNewUrlParser: true,
+//  useUnifiedTopology: true,
+//})
+//.then(() => {
+//  console.log('Connected to MongoDB : ', process.env.MONGO_URI);
+
+  // Démarrer le serveur une fois connecté à MongoDB
+  server.listen(port, () => {
+    console.log('Server is running on port ' + port);
+  });
+//})
+//.catch(err => {
+//  console.error('Failed to connect to MongoDB', err);
+//  process.exit(1); // Arrêter le processus si la connexion échoue
+//});
