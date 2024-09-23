19fb45bda98c0823c3208040f4b27123b0a40bf2
MayeultheLink <mdelaserre@hotmail.fr>
2024-09-23 15:29:37 +0200

    frontend frame choice

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         return;
     }
 
+    // get user infos
     try {
         const response = await fetch("/api/user", {
             method: "GET",
@@ -26,11 +27,13 @@ document.addEventListener('DOMContentLoaded', async () => {
         window.location.href = 'signin.html';
     }
 
+    // logout
     document.getElementById("logout").addEventListener("click", () => {
         localStorage.removeItem("token");
         window.location.href = "signin.html";
     })
 
+    // launch video
     const video = document.getElementById("video");
     navigator.mediaDevices.getUserMedia({ video: true })
         .then((stream) => {
@@ -39,14 +42,35 @@ document.addEventListener('DOMContentLoaded', async () => {
         .catch((err) => {
             console.error("Error while accessing camera: ", err);
         })
+
+    // select frame
+    const webcamContainer = document.getElementById("webcam-container");
+    const squareFrameButton = document.getElementById("square-frame-button");
+    const circleFrameButton = document.getElementById("circle-frame-button");
+    const frames = ["https://res.cloudinary.com/dx8krot13/image/upload/v1726842282/ab088edf1303b946bb01e6bd670eb81c_fgcxnl.png", "https://res.cloudinary.com/dx8krot13/image/upload/v1726840089/cchqkqelrzkdwlya4zx5.png"];
+    const frame = document.createElement("img");
+    squareFrameButton.addEventListener("click", () => {
+        appendFrame(frame, frames[0]);
+        webcamContainer.appendChild(frame);
+    })
+    circleFrameButton.addEventListener("click", () => {
+        appendFrame(frame, frames[1]);
+        webcamContainer.appendChild(frame);
+    })
     
+    // print picture
     const canvas = document.getElementById("canvas");
     const captureButton = document.getElementById("capture");
     captureButton.addEventListener("click", () => {
+        if (!frame.src) {
+            alert("Please select a frame before taking a picture");
+            return;
+        }
         const context = canvas.getContext("2d");
         context.drawImage(video, 0, 0, canvas.width, canvas.height);
     })
 
+    // post picture
     const canvasForm = document.getElementById("canvas-form");
     canvasForm.addEventListener("submit", async (event) => {
         event.preventDefault();
@@ -79,6 +103,52 @@ document.addEventListener('DOMContentLoaded', async () => {
                 alert(error.message);
             }
     });
+
+    // append frames
+    const appendFrame = (img, frameUrl) => {
+        img.src = frameUrl;
+        img.alt = "Frame";
+        img.width = "360";
+        img.height = "240";
+        img.style.position = "absolute";
+        img.style.top = 0;
+        img.style.left = 0;
+        img.style.width = "100%";
+        img.style.height = "100%";
+        img.style.zIndex = "2";
+        img.style.pointerEvents = "none";
+    }
+
+    // document.getElementById('imageInput').addEventListener('change', async (event) => {
+    //     const file = event.target.files[0];
+    //     const maxSizeInBytes = 5 * 1024 * 1024; // 5 Mo
+    
+    //     if (file.size > maxSizeInBytes) {
+    //         alert('File is too big, limit is 5 Mo');
+    //         event.target.value = ''; // Réinitialiser l'input
+    //         return;
+    //     }
+    //     try {
+    //         const response = await fetch("/api/pictures", {
+    //             method: "POST",
+    //             headers: {
+    //                 "Authorization": `Bearer ${token}`,
+    //                 "Content-Type": "application/json"
+    //             },
+    //             body: JSON.stringify({ image: imageDataUrl })
+    //         });
+    
+    //         if (response.ok) {
+    //             alert("Picture uploaded");
+    //         } else {
+    //             const data = response.json();
+    //             alert(data.message);
+    //         }
+    
+    //     } catch (error) {
+    //         alert(error.message);
+    //     }
+    // });
 });
 
 function isCanvasEmpty(canvas) {
